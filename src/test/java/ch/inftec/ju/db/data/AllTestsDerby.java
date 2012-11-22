c436afdd41a90ca818fe444edfb396026b69431a
Martin Meyer <martin.meyer@inftec.ch>
2012-11-22 21:00:48 +0100

    Completed first version with Derby test case

---
+++ src/test/java/ch/inftec/ju/db/data/AllTestsDerby.java
@@ -0,0 +1,13 @@
+package ch.inftec.ju.db.data;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import ch.inftec.ju.db.specific.DerbyTests;
+
+@RunWith(Suite.class)
+@SuiteClasses({ DerbyTests.class })
+public class AllTestsDerby {
+
+}
