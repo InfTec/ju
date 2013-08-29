411489a67a80b00f41498d0c5b4d5129e2e0a65d
Martin Meyer <martin.meyer@inftec.ch>
2013-08-29 21:32:35 +0200

    Restructured test projects
    Added empty test projects ju-dbutil-test and ju-dbutil-test-derby

--- ju-dbutil-test/src/test/oracle/java/ch/inftec/ju/db/specific/OracleDbChangeSetTest.java
+++
@@ -1,9 +0,0 @@
-package ch.inftec.ju.db.specific;
-
-import org.springframework.test.context.ContextConfiguration;
-
-import ch.inftec.ju.db.change.DbChangeSetTest;
-
-@ContextConfiguration(locations="classpath:/ch/inftec/ju/db/specific/DbTestsOracle-context.xml", inheritLocations=false)
-public class OracleDbChangeSetTest extends DbChangeSetTest {
-}
--- ju-dbutil-test/src/test/oracle/java/ch/inftec/ju/db/specific/OracleJuDbUtilsTest.java
+++
@@ -1,9 +0,0 @@
-package ch.inftec.ju.db.specific;
-
-import org.springframework.test.context.ContextConfiguration;
-
-import ch.inftec.ju.db.JuDbUtilsTest;
-
-@ContextConfiguration(locations="classpath:/ch/inftec/ju/db/specific/DbTestsOracle-context.xml", inheritLocations=false)
-public class OracleJuDbUtilsTest extends JuDbUtilsTest {
-}
