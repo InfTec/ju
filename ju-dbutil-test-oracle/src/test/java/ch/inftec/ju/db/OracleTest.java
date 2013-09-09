8840a801baef055b5ffaafb9516d09c941f1af00
Martin Meyer <martin.meyer@inftec.ch>
2013-09-09 04:08:05 +0200

    Updated drivers, having troubles with Oracle update using Liquibase

--- ju-dbutil-test-oracle/src/test/java/ch/inftec/ju/db/OracleTest.java
+++ ju-dbutil-test-oracle/src/test/java/ch/inftec/ju/db/OracleTest.java
@@ -1,5 +1,13 @@
 package ch.inftec.ju.db;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.persistence.Query;
+
+import org.hibernate.jdbc.Work;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -15,5 +23,24 @@ public class OracleTest extends AbstractDbTest {
 	@Test
 	public void dbType_isReckognized() {
 		Assert.assertEquals(DbType.ORACLE, this.emUtil.getDbType());
+	}
+	
+	@Test
+	public void canEvaluateDefaultSchema() {
+		Query qry = this.em.createNativeQuery("select sys_context( 'userenv', 'current_schema' ) from dual");
+		Object res = qry.getSingleResult();
+		Assert.assertNotNull(res);
+	}
+	
+	@Test
+	public void canEvaluateDefaultSchema_usingPrepareCall() {
+		this.emUtil.doWork(new Work() {
+			@Override
+			public void execute(Connection connection) throws SQLException {
+				CallableStatement cs = connection.prepareCall("select sys_context( 'userenv', 'current_schema' ) from dual");
+				ResultSet rs = cs.executeQuery();
+				rs.next();
+			}
+		});
 	}
 }
